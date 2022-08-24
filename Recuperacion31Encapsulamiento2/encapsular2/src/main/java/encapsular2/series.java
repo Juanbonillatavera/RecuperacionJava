6e0b9f2c16e816e22eea43f0b8d15388b242050e
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-08-23 23:32:31 -0500

    actualizacion de codigo

---
+++ Recuperacion31Encapsulamiento2/encapsular2/src/main/java/encapsular2/series.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package encapsular2;
+
+/**
+ *
+ * @author juan bonilla
+ */
+public class series extends media{ // clase hija de media
+
+    public series(int totalEpisodes, int totalSeason, String name, String genre, int duration) {  super(name, genre, duration);
+        this.totalEpisodes = totalEpisodes;     
+        this.totalSeason = totalSeason;
+    }
+        
+    public int getTotalEpisodes() {
+        // metodos
+        return totalEpisodes;
+    }
+
+    public void setTotalEpisodes(int totalEpisodes) {
+        this.totalEpisodes = totalEpisodes;
+    }
+
+    public int getTotalSeason() {
+        return totalSeason;
+    }
+
+    public void setTotalSeason(int totalSeason) {
+        this.totalSeason = totalSeason;
+    }
+    
+   private int totalEpisodes; // encapsulamiento de datos con privatte ahora no se podran usar solo con get setter
+   private int totalSeason;
+    
+}
